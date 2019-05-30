4080b9976aafdda5b5e5d6feac57d54ead9e270a
Kornel Prokopowicz <33931912+FlayJalapeno@users.noreply.github.com>
2019-05-30 17:37:20 +0200

    ui v2

--- mainwindow.h
+++ mainwindow.h
@@ -14,9 +14,18 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    int currInd = 0;
+    bool wykrJ = false;
+    bool wykrE = false;
 
 private slots:
     void on_rysujWykres_clicked();
+
+    void on_pierwiastkiBox_currentIndexChanged(int index);
+
+    void on_wykresJ_toggled(bool checked);
+
+    void on_wykresE_toggled(bool checked);
 
 private:
     Ui::MainWindow *ui;
