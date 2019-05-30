cc19c8122f4b56e93b1d7020bf8d8a59701243a4
Kornel Prokopowicz <33931912+FlayJalapeno@users.noreply.github.com>
2019-05-30 17:37:49 +0200

    ui v2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "Decay.h"
+
 
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -17,7 +19,33 @@ MainWindow::~MainWindow()
 void MainWindow::on_rysujWykres_clicked()
 {
 
+    switch (currInd) { //wybrany pierwiastek
+    case 0:
+        Decay Polon210(1000,11955686.4,209.9828737,205.9744653,true);
+        if(wykrJ == true){
+            Polon210.NucleiOverTimeDisc();
+            QLineSeries *jednSeries = new QLineSeries();
+            for(int i = 0; i < Polon210.decayVec.size(); i++){
+                jednSeries->append(i*Polon210.CalculateStep(),Polon210.decayVec[i]);
+            }
+        }
+
+    }
 }
 
 
 
+void MainWindow::on_pierwiastkiBox_currentIndexChanged(int index)
+{
+    currInd = index;
+}
+
+void MainWindow::on_wykresJ_toggled(bool checked)   //czy rysować wykres jednostek
+{
+    wykrJ = checked;
+}
+
+void MainWindow::on_wykresE_toggled(bool checked)   //czy rysować wykres energii
+{
+    wykrE = checked;
+}
